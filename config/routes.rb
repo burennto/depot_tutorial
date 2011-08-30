7e9d22aa22c7ccbc138937bc5689f4d1db9e445a
Brent Chuang <burennto@gmail.com>
2011-08-30 21:39:59 +1000

    Modified root route and removed index.html

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 DepotTutorial::Application.routes.draw do
+  
+  root :to => 'store#index', :as => 'store'
+  
+  get "store/index"
+
   resources :products
 
   # The priority is based upon order of creation:
